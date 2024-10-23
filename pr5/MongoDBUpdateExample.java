0de9a8973478034cd7a0b334a41944c7204f2ed1
dendra19 <143658440+dendra19@users.noreply.github.com>
2024-10-23 23:58:45 +0530

    Update MongoDBUpdateExample.java

--- pr5/MongoDBUpdateExample.java
+++ pr5/MongoDBUpdateExample.java
@@ -1,23 +1,31 @@
 import java.net.UnknownHostException;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
+
+
 import com.mongodb.MongoClient;
 import com.mongodb.WriteResult;
 
-public class MongoDBUpdateExample {
-    public static void main(String[] args) throws UnknownHostException {
-        MongoClient mongo = new MongoClient("localhost", 27017); 
-        System.out.println("Connected to the database successfully");
+public class Update
+{
+	public static void main(String [] args)
+	{
+		MongoClient mongo = new MongoClient("localhost",27017);
+		DB db= mongo.getDB("College");
+		
+		DBCollection col = db.getCollection("teacher");
+		
+		DBObject query = new BasicDBObject("id",1);
+		DBObject update = new BasicDBObject();
 
-        MongoDatabase database = mongo.getDatabase("myDb1"); 
+		update.put("$set",new BasicDBObject("likes",10000));
+		WriteResult result = col.update(query, update);
+		mongo.close();
 
-        MongoCollection<Document> collection = database.getCollection("sampleCollection1");
-        System.out.println("Collection sampleCollection1 selected successfully");
-
-        collection.deleteOne(Filters.eq("id", 1)); 
-        System.out.println("Document deleted successfully...");
-
-    }
+        System.out.println("Document Updated Successfully");
+		
+	}
 }
